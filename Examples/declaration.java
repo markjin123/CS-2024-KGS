6cecb727e191430219f6675261f9d4d555d1e97f
Mark Jin <mark.jin@tulip.com>
2024-09-03 22:26:29 -0400

    add assignment

---
+++ Examples/declaration.java
@@ -0,0 +1,15 @@
+class Example {
+    public static void main(String[] args) {
+        int a = 4;
+        boolean b = false;
+        float c = 5.6f;
+        double d = 5.3;
+        char f = 'a';
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+    }
+}
